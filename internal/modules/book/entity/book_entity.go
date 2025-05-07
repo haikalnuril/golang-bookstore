a8b5e79e4db6c5daf800a186be40667755ce4ab4
haikalnuril <haikal.nuril23@gmail.com>
2025-05-07 20:56:09 +0700

    feat: entity book in database

--- internal/modules/book/entity/book_entity.go
+++ internal/modules/book/entity/book_entity.go
@@ -1,1 +1,17 @@
 package entity
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type Book struct {
+	ID            uuid.UUID `gorm:"type:uuid;default:uuid_generate_v4()" json:"id"`
+	Title         string    `json:"title" validate:"required,min=2"`
+	Author        string    `json:"author" validate:"required,min=2"`
+	Genre         string    `json:"genre" validate:"required,min=2"`
+	PublishedYear int       `json:"published_year" validate:"required,min=1900,max=2025"`
+	CreatedAt     time.Time
+	UpdatedAt     time.Time
+}
--- internal/modules/book/repository/postgres/book_postgres.go
+++ internal/modules/book/repository/postgres/book_postgres.go
@@ -1,1 +1,28 @@
 package postgres
+
+import (
+	"bookstore/internal/modules/book/entity"
+
+	"gorm.io/gorm"
+)
+
+type bookPostgres struct {
+	db *gorm.DB
+}
+
+func NewBookPostgres(db *gorm.DB) *bookPostgres {
+	return &bookPostgres{db: db}
+}
+
+func (r *bookPostgres) Create(book *entity.Book) error {
+	return r.db.Create(book).Error // r stand for repository
+}
+
+func (r *bookPostgres) GetAll() ([]entity.Book, error) {
+	books := []entity.Book{}
+	err := r.db.Find(&books).Error
+	if err != nil {
+		return nil, err
+	}
+	return books, nil
+}
